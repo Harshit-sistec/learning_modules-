eeb1a76f0e987a09f498d95b9f39f2c174e411cc
Harshit-sistec <harshitdubeytd@gmail.com>
2025-08-26 12:55:08 +0530

    Create day4.js

---
+++ day4.js
@@ -0,0 +1,14 @@
+// Import Express
+const express = require('express');
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
+
